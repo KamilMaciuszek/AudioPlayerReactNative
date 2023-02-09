8ba6c75664aa7c361a5f74eab035fd24ba666283
kamil.maciuszek@gmail.com <Kamil.maciuszek@gmail.com>
2023-02-09 16:01:44 +0100

    Better behaviour on the bigger screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -46,15 +46,17 @@ function RegisterScreen() {
         colors={[Colors.primaryGrey, Colors.primaryRed]}
         style={styles.gradient}
       >
-        <Image
-          source={require("../assets/icon.png")}
-          style={styles.image}
-        ></Image>
-        <Title textStyle={styles.titleStyle}>Sign Up</Title>
         <KeyboardAvoidingView
           style={{ alignItems: "stretch" }}
           behavior="position"
         >
+        <Image
+          source={require("../assets/icon.png")}
+          style={styles.image}
+        ></Image>
+        
+        <Title textStyle={styles.titleStyle}>Sign Up</Title>
+        
           <Text style={styles.hint}>Name</Text>
 
           <TextInput
@@ -124,6 +126,7 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     marginBottom: "10%",
+    alignSelf: "center"
   },
   titleStyle: {
     marginBottom: "20%",
